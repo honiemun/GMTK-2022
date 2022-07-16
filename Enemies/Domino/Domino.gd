7224a89f00c2bfec6d9d37028768753478ed425c
Honie <87244806+honiemun@users.noreply.github.com>
2022-07-16 10:49:25 -0300

    Knockback & kill

--- Enemies/Domino/Domino.gd
+++ Enemies/Domino/Domino.gd
@@ -5,6 +5,7 @@ var velocity: Vector2 = Vector2.ZERO
 var path: Array = []	# Contains destination positions
 var levelNavigation: Navigation2D = null
 var player = null
+var dice = null
 var player_spotted: bool = false
 
 onready var los = $LineOfSight
@@ -15,8 +16,15 @@ onready var enemyHandler = tree.get_nodes_in_group("EnemyHandler")[0]
 var last_spotted_player_pos: Vector2 = Vector2.ZERO
 var reached_last_player_pos: bool = true
 
+var last_pos = global_position
+
 var diceRoll		# The life this enemy has.
 const MAX_ROLL = 6
+
+var knockback = Vector2.ZERO
+const friction = 200
+const knockback_power = 250
+var isKnocked = false
 
 func _ready():
 	# Roll for STATS
@@ -30,20 +38,29 @@ func _ready():
 	if tree.has_group("Player"):
 		player = tree.get_nodes_in_group("Player")[0]
 		last_spotted_player_pos = player.global_position
+	if tree.has_group("Dice"):	
+		dice = tree.get_nodes_in_group("Dice")[0]
 
-func _physics_process(_delta):
-	if player:
-		los.look_at(player.global_position)
-		check_player_in_detection()
-		if player_spotted:
-			last_spotted_player_pos = player.global_position
-			reached_last_player_pos = false
-			generate_path()
-			navigate()
-		elif !reached_last_player_pos:
-			navigate_to_old_position()
-	if !reached_last_player_pos:
-		move()
+func _physics_process(delta):
+	if !isKnocked:
+		if player:
+			los.look_at(player.global_position)
+			check_player_in_detection()
+			if player_spotted:
+				last_spotted_player_pos = player.global_position
+				reached_last_player_pos = false
+				generate_path()
+				navigate()
+			elif !reached_last_player_pos:
+				navigate_to_old_position()
+		if !reached_last_player_pos:
+			move()
+	else:
+		knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
+		knockback = move_and_slide(knockback)
+		if global_position == last_pos:
+			isKnocked = false
+		last_pos = global_position
 
 # PATHFINDING FUNCTIONS
 
@@ -75,3 +92,12 @@ func generate_path():	# It's obvious
 
 func move():
 	velocity = move_and_slide(velocity.floor())
+
+func _on_Hurtbox_area_entered(area):
+	# If dice is lower than life:
+	if dice.state != dice.FOLLOW:
+		if dice.rawDiceRoll > diceRoll:
+			queue_free()
+		else:
+			isKnocked = true
+			knockback = dice.global_position.direction_to(global_position) * knockback_power
