996c06777c000062ceedac69fd243b648da9fc7a
Honie <87244806+honiemun@users.noreply.github.com>
2022-07-16 14:22:19 -0300

    Enemy pathfinding code abstracted

--- Enemies/Domino/Domino.gd
+++ Enemies/Domino/Domino.gd
@@ -43,55 +43,13 @@ func _ready():
 
 func _physics_process(delta):
 	if !isKnocked:
-		if player:
-			los.look_at(player.global_position)
-			check_player_in_detection()
-			if player_spotted:
-				last_spotted_player_pos = player.global_position
-				reached_last_player_pos = false
-				generate_path()
-				navigate()
-			elif !reached_last_player_pos:
-				navigate_to_old_position()
-		if !reached_last_player_pos:
-			move()
+		enemyHandler.follow_player(self, player)
 	else:
 		knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 		knockback = move_and_slide(knockback)
 		if global_position == last_pos:
 			isKnocked = false
 		last_pos = global_position
-
-# PATHFINDING FUNCTIONS
-
-func check_player_in_detection() -> bool:
-	var collider = los.get_collider()
-	if collider and collider.is_in_group("Player"):
-		player_spotted = true
-		return true
-	player_spotted = false
-	return false
-
-func navigate():	# Define the next position to go to
-	if path.size() > 1:
-		velocity = global_position.direction_to(path[1]) * SPEED
-
-		# If reached the destination, remove this point from path array
-		if global_position.distance_to(path[0]) < 1:
-			path.pop_front()
-
-func navigate_to_old_position():
-	velocity = global_position.direction_to(last_spotted_player_pos) * SPEED
-
-	if global_position.distance_to(last_spotted_player_pos) < 1:
-		reached_last_player_pos = true
-
-func generate_path():	# It's obvious
-	if levelNavigation != null and player != null:
-		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
-
-func move():
-	velocity = move_and_slide(velocity.floor())
 
 func _on_Hurtbox_area_entered(area):
 	# If dice is lower than life:
--- World/EnemyHandler.gd
+++ World/EnemyHandler.gd
@@ -4,7 +4,52 @@ var horde = 1
 var difficulty = 2 # Decreases slightly for every horde, making rolls higher.
 
 var rng = RandomNumberGenerator.new()
+var last_spotted_player_pos: Vector2 = Vector2.ZERO
+var reached_last_player_pos: bool = true
 
 func roll_enemy_stats(maxRoll, difficultyOn):
 	maxRoll = (round(maxRoll / difficulty) if difficultyOn else maxRoll)
 	return rng.randi_range(1, maxRoll)
+
+# PATHFINDING AND NAVIGATION
+
+func follow_player(enemy, player):
+	if player:
+		enemy.los.look_at(player.global_position)
+		var player_spotted = check_player_in_detection(enemy)
+		if player_spotted:
+			last_spotted_player_pos = player.global_position
+			reached_last_player_pos = false
+			var path = generate_path(enemy, player)
+			navigate(enemy)
+		elif !reached_last_player_pos:
+			navigate_to_old_position(enemy)
+		if !reached_last_player_pos:
+			move(enemy, enemy.velocity)
+
+func check_player_in_detection(enemy) -> bool:
+	var collider = enemy.los.get_collider()
+	if collider and collider.is_in_group("Player"):
+		return true
+	return false
+
+func generate_path(enemy, player):	# It's obvious
+	if enemy.levelNavigation != null and player != null:
+		return enemy.levelNavigation.get_simple_path(enemy.global_position, player.global_position, false) as Array
+
+func navigate(enemy):	# Define the next position to go to
+	if enemy.path.size() > 1:
+		enemy.velocity = enemy.global_position.direction_to(enemy.path[1]) * enemy.SPEED
+
+		# If reached the destination, remove this point from path array
+		if enemy.global_position.distance_to(enemy.path[0]) < 1:
+			enemy.path.pop_front()
+
+func navigate_to_old_position(enemy):
+	enemy.velocity = enemy.global_position.direction_to(last_spotted_player_pos) * enemy.SPEED
+
+	if enemy.global_position.distance_to(last_spotted_player_pos) < 1:
+		reached_last_player_pos = true
+
+func move(enemy, velocity):
+	enemy.velocity = enemy.move_and_slide(velocity.floor())
